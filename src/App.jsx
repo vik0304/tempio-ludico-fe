1f703c832d27c23c2325eddce2ce05599d853dc6
Viktor Vasileski <viktor.vasileski1@gmail.com>
2025-11-05 21:24:32 +0100

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,10 @@
-import { useState } from "react";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p className="text-3xl font-bold underline bg-sky-500/80">
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar></Navbar>
     </>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+function Navbar() {
+  return (
+    <div>
+      <p>Home</p>
+      <p>Eventi</p>
+      <p>Chi siamo</p>
+      <p>Contatti</p>
+      <p>LOGOQUI</p>
+      <p>Cerca</p>
+      <p>I nostri giochi</p>
+      <p>Wishlist</p>
+      <p>Utente</p>
+    </div>
+  );
+}
+
+export default Navbar;
