9a4036f489a8c0c7065a7544a8b51a4edc68458a
Viktor Vasileski <viktor.vasileski1@gmail.com>
2025-11-07 20:23:16 +0100

    updates navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,34 @@
+import logo from "../assets/itl_logo_tondo.png";
+
 function Navbar() {
   return (
-    <div>
-      <p>Home</p>
-      <p>Eventi</p>
-      <p>Chi siamo</p>
-      <p>Contatti</p>
-      <p>LOGOQUI</p>
-      <p>Cerca</p>
-      <p>I nostri giochi</p>
-      <p>Wishlist</p>
-      <p>Utente</p>
+    <div className="flex flex-row justify-around w-screen p-4 bg-red-te">
+      <div className="hover:cursor-pointer content-center">
+        <p>Home</p>
+      </div>
+      <div className="hover:cursor-pointer content-center">
+        <p>Eventi</p>
+      </div>
+      <div className="hover:cursor-pointer content-center">
+        <p>Chi siamo</p>
+      </div>
+      <div className="hover:cursor-pointer content-center">
+        <p>Contatti</p>
+      </div>
+      <div className="hover:cursor-pointer content-center">
+        <img
+          src={logo}
+          alt="Logo circolare del tempio ludico"
+          className="h-12"
+        />
+      </div>
+      <div className="hover:cursor-pointer content-center">
+        <p>Cerca</p>
+      </div>
+      <div className="hover:cursor-pointer content-center">
+        <p>I nostri giochi</p>
+      </div>
+      <div className="hover:cursor-pointer content-center">Utente</div>
     </div>
   );
 }
